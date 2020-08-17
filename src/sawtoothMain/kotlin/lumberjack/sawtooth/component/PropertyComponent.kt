154cd4602b49449f630dca47d2a567bb49d6640b
Izzy <undermybrella@abimon.org>
2020-08-17 10:35:57 +1000

    Add licensing and newlines throughout, change componentRegistry to be public and mutable, remove LambdaLogProperty and make it a function, and add documentation to PatternLayout
    Signed-off-by: Izzy <undermybrella@abimon.org>

--- src/sawtoothMain/kotlin/lumberjack/sawtooth/component/PropertyComponent.kt
+++ src/sawtoothMain/kotlin/lumberjack/sawtooth/component/PropertyComponent.kt
@@ -1,17 +1,27 @@
+/*
+ * This file is part of Lumberjack.
+ *
+ * Lumberjack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Lumberjack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Lumberjack.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package lumberjack.sawtooth.component
 
 import lumberjack.sawtooth.event.LogEvent
 import lumberjack.sawtooth.event.PropertyKey
 
 open class PropertyComponent<T : Any>(val property: PropertyKey<T>) : PatternComponent {
-    private var formatOptions: List<String> = emptyList()
-
     override fun writeTo(builder: StringBuilder, event: LogEvent) {
         builder.append(event.property(property)?.let(this::format))
-    }
-
-    override fun tune(modifiers: List<String>) {
-        formatOptions = modifiers
     }
 
     open fun format(property: T): String = property.toString()
--- src/sawtoothMain/kotlin/lumberjack/sawtooth/event/LambdaLogProperty.kt
+++
@@ -1,9 +0,0 @@
-package lumberjack.sawtooth.event
-
-class LambdaLogProperty<T : Any>(override val key: PropertyKey<T>, val supply: (LogEvent) -> T?) : LogProperty<T> {
-    companion object {
-        inline operator fun <reified T : Any> invoke(name: String, noinline supply: (LogEvent) -> T?): LambdaLogProperty<T> = LambdaLogProperty(PropertyKey(name, T::class), supply)
-    }
-
-    override fun value(event: LogEvent): T? = supply(event)
-}
